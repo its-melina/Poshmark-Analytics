340e1836d35498d939cd150fe2bc22655d5d6a2f
Melina <melinafaraone@gmail.com>
2025-06-14 22:43:41 -0700

    SQL data cleaning

--- data-cleaning.sql
+++ data-cleaning.sql
@@ -4,15 +4,15 @@
 
 ALTER table tops ALTER column sold_date TYPE date USING TO_DATE(sold_date, 'MM-DD-YYYY');
 
--- 2. change price data to integer datatype
 
+-- 2. change price data to integer datatype
 
 UPDATE tops SET sold_price = REPLACE(REPLACE(sold_price, '$', ''), ',', '');
 UPDATE tops SET original_price = REPLACE(REPLACE(original_price, '$', ''), ',', '');
 
 ALTER table tops ALTER column sold_price TYPE integer USING sold_price::integer;
 ALTER table tops ALTER column original_price TYPE integer USING original_price::integer;
-*/
+
 
 -- 3. add column for % of original price a listing sold for
 
@@ -22,6 +22,14 @@ ALTER table tops
 UPDATE tops
 	SET proportion_original_price = ROUND(CAST(sold_price as NUMERIC)/original_price, 2);
 
+
+-- 4. changing binary columns to integers for clarity
+
+ALTER table tops ALTER column nwt TYPE integer USING nwt::integer;
+ALTER table tops ALTER column boutique TYPE integer USING boutique::integer;
+
+
+-- view table 
 
 SELECT * from tops
 	WHERE proportion_original_price is NOT NULL AND proportion_original_price < 1
